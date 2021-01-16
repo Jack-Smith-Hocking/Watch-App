70292295b283d66ff907660510b397f0d6b27fa3
Jack Smith-Hocking <s191037@students.aie.edu.au>
2021-01-16 22:03:44 +1030

    Worked on PC build
    - Set up icons for mobile and PC
    - Set up and tested PC build

---
+++ Watch App/Assets/Scripts/Utility/EventWrapper.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Custom.Utility
+{
+    public class EventWrapper : MonoBehaviour
+    {
+        public void SetTmeScale(float timeScale)
+        {
+            Time.timeScale = timeScale;
+        }
+
+        public void ToggleScript(MonoBehaviour mono)
+        {
+            if (mono != null)
+            {
+                mono.enabled = !mono.enabled;
+            }
+        }
+
+        public void ToggleTarget(GameObject target)
+        {
+            if (target != null)
+            {
+                target.SetActive(!target.activeInHierarchy);
+            }
+        }
+        public void ToggleSelf()
+        {
+            ToggleTarget(gameObject);
+        }
+
+        public void LoadSceneIndex(int index)
+        {
+            if (index > 0 && index <= SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(index);
+            }
+        }
+        public void LoadSceneName(string sceneName)
+        {
+            if (sceneName.Length == 0) return;
+
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void ReloadScene() 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void DestroyTarget(GameObject target)
+        {
+            if (target != null)
+            {
+                Destroy(target);
+            }
+        }
+
+        public void DestroySelf()
+        {
+            DestroyTarget(gameObject);
+        }
+
+        public void LockCursor()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        public void ConfineCursor()
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        public void ResetCursor()
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        public void SetCursorVisibility(bool isVisible)
+        {
+            Cursor.visible = isVisible;
+        }
+    }
+}
